200715c401dad68892e2d9b002d598b9b6febad9
Yusuf Khan <5564948+YusufKhan@users.noreply.github.com>
2024-10-22 09:59:13 +0200

    Move weights starting table. May not be using correct table for calcs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
 'use client';
-import weightsStartingTable from '..//lib/weightsStartingTable'
+//import weightsStartingTable from '..//lib/weightsStartingTable'
 import React, { useState, useEffect } from 'react';
 
 interface Teams {
   [charName: string]: number[];
+}
+
+class weightsStartingTable {
+  public teams: Teams;
+
+  constructor() {
+      this.teams = {
+          Alhaitham: [0, 0.3, 0, 0, 0.8, 0, 2.2, 2.3, 0, 1.1],
+          Arlecchino: [0, 0.49, 0, 0, 1.51, 0, 2.95, 2.33, 0, 1.33],
+          'Hu Tao': [0.62, 0.5, 0, 1.88, 1.07, 0, 3.2, 2.16, 0, 2.36],
+          Shougun: [0, 0.53, 0, 0, 1.34, 0, 2.92, 2.43, 1.1, 0],
+          Fischl: [0, 0.63, 0, 0, 1.41, 0, 2.62, 2.88, 0, 1.43],
+          Furina: [0.93, 0, 0, 2.78, 0, 0, 1.19, 2.58, 0, 0],
+          Eula: [0, 0.79, 0, 0, 2.4, 0, 1.57, 2.36, 0, 0]
+      };
+  }
+
+  getTeams(): Teams {
+      return this.teams;
+  }
+
+  getTeam(charName: string): number[] | undefined {
+      return this.teams[charName];
+  }
+
+  updateTeam(charName: string, stats: number[]): void {
+      this.teams[charName] = stats;
+  }
 }
 
 const HomePage = () => {
@@ -36,7 +64,7 @@ const HomePage = () => {
 
       //Separate from above so they can be cleared independently
       if ((savedWeights !== undefined && savedWeights !== null && savedWeights !== "undefined")) {
-        setWeights(JSON.parse(savedWeights).teams);
+        setWeights(JSON.parse(savedWeights));
         console.log(savedWeights);
       } else {
         console.info('No weights table saved');
@@ -89,6 +117,8 @@ const HomePage = () => {
 
       localStorage.setItem('uid', uid); // Cache the UID
       localStorage.setItem('characterData', JSON.stringify(data));
+
+      console.log(weights);
       localStorage.setItem('weights', JSON.stringify(weights));
 
     } catch (error) {
