31f9bb9154b8e6354dd63db10529206d471e9ab5
Yusuf Khan <ykhan@pas-sa.co.za>
2024-10-24 17:30:29 +0200

    Copy cache from node_module instead of downloading

--- pages/api/getCharacterData.ts
+++ pages/api/getCharacterData.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { EnkaManager, Client } from "genshin-manager";
 import fs from "fs";
+import path from "path";
 
 let client: Client | null = null;
 
@@ -36,18 +37,34 @@ export default async function handler(
 
 const getClient = async () => {
   if (!client) {
-    const tempDir = "/tmp/genshin-manager-cache";
+    const tempDir = "/tmp/cache";
     // Ensure the directory exists
-    if (!fs.existsSync(tempDir)) {
+    /* if (!fs.existsSync(tempDir)) {
       fs.mkdirSync(tempDir, { recursive: true });
       console.log("Temp directory created:", tempDir); // Does it persist for this serverless function
-    }
+    } */
+
+    const sourceFolder = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "node_modules",
+      "genshin-manager",
+      "cache"
+    );
+    const targetFolder = "/tmp/";
+
+    console.time("Copying cache folder to temp");
+    copyFolderRecursiveSync(sourceFolder, targetFolder);
+    console.timeEnd("Copying cache folder to temp");
 
     client = new Client({
-      assetCacheFolderPath: "/tmp/genshin-manager-cache",
+      assetCacheFolderPath: tempDir,
       downloadLanguages: ["EN"],
       showFetchCacheLog: true,
-      autoFetchLatestAssetsByCron: "10 15 * * 4", // Every Wednesday at midnight
+      autoFetchLatestAssetsByCron: "0 0 * * 3", // Every Wednesday at midnight
       fetchOption: {
         headers: {
           "User-Agent": "genshin-artifact-appraiser.vercel.app",
@@ -60,4 +77,22 @@ const getClient = async () => {
     console.timeEnd("ClientUpdate");
   }
   return client;
+};
+
+const copyFolderRecursiveSync = (source: string, target: string) => {
+  const targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder, { recursive: true });
+  }
+
+  if (fs.lstatSync(source).isDirectory()) {
+    fs.readdirSync(source).forEach((file) => {
+      const curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        fs.copyFileSync(curSource, path.join(targetFolder, file));
+      }
+    });
+  }
 };
