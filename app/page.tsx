cfeaf28248aafee88520d9284d6fec1b7ed87e7a
Yusuf Khan <ykhan@pas-sa.co.za>
2024-10-28 00:34:51 +0200

    Wrapper removal. Server API call directly to Enka.Network

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { CharacterDetail } from "genshin-manager";
 import React, { useState, useEffect } from "react";
 import { calculateRVs } from "@/utils/calcRVs";
-import {
-  handleChange,
-  getCharacterData,
-  onClick,
-  WeightsTable,
-} from "@/utils/homeUtils";
+import { handleChange } from "@/utils/handleChange";
+import { fetchEnkaData } from "@/utils/fetchEnkaData";
+import { WeightsData, UserData, CharacterRollValues } from "@/utils/interfaces";
+
+const weightsFile = "/startingWeights.json";
 
 const HomePage = () => {
   // =========================
@@ -15,90 +13,99 @@ const HomePage = () => {
   // =========================
 
   const [loading, setLoading] = useState(false); // Loading circle
-  const [uid, setUid] = useState(""); // UID fed to getCharacterData
-  const [characterData, setCharacterData] = useState<CharacterDetail[]>([]); // Received from getCharacterData
-  const [weights, setWeights] = useState<WeightsTable>({}); // For resetting weights with button
-  const [rollValues, setRollValues] = useState<(string | number)[][]>([]); // The final character RV table
+  const [uid, setUid] = useState(""); // UID ultimately fed to fetchEnkaData
+  const [userData, setUserData] = useState<UserData>(); // Received from lib/enkaFetch
+  const [weights, setWeights] = useState<WeightsData>({}); // For resetting weights with button
+  const [rollValues, setRollValues] = useState<CharacterRollValues[]>([]); // The final character RV table
   const [error, setError] = useState("");
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false); // What is this button
   const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
-    const savedUID = localStorage.getItem("uid") || "";
-    setUid(savedUID);
-    if (savedUID) {
-    } else {
-    }
-    const savedCharacterData = localStorage.getItem("characterData") || "";
-    if (savedCharacterData) {
-      setCharacterData(JSON.parse(savedCharacterData));
-    } else {
-      setCharacterData([]);
-    }
-    const savedRVs = localStorage.getItem("rollValues") || "";
-    if (savedRVs) {
-      setRollValues(JSON.parse(savedRVs));
-    } else {
-      setRollValues([]);
-    }
-    const weightsTable = new weightsStartingTable();
-    const savedWeights = localStorage.getItem("weights");
-    if (savedWeights) {
-      setWeights(JSON.parse(savedWeights));
-    } else {
-      setWeights(weightsTable.teams);
-    }
+    // Try and get data from browser local storage
+    setUid(localStorage.getItem("uid") || "");
+    setWeights(JSON.parse(localStorage.getItem("weights") || "{}"));
+    setRollValues(JSON.parse(localStorage.getItem("rollValues") || "[]"));
+    setUserData(JSON.parse(localStorage.getItem("userData") || "[]"));
+
+    const loadWeights = async () => {
+      // Set to starting weights from JSON file, if nothing stored locally
+      const savedWeights = localStorage.getItem("weights") || "";
+      if (savedWeights) {
+        setWeights(JSON.parse(savedWeights));
+      } else {
+        handleResetWeights();
+      }
+    };
+    loadWeights();
   }, []);
 
   // =========================
   // Event Handlers
   // =========================
 
-  const handleSubmit = async () => {
-    const data = await getCharacterData(uid, setUid, setError, setLoading);
-    const newRollValues = calculateRVs(data, weights);
+  const handleSubmit = async (newUID: string, newWeights: WeightsData) => {
+    console.time("handleSubmit");
+    localStorage.setItem("uid", uid);
+    setUid(uid);
+    setError("");
+    setLoading(true);
+    try {
+      const newUserData = (await fetchEnkaData(newUID)) as UserData;
+      localStorage.setItem("userData", JSON.stringify(newUserData));
+      setLoading(false);
+      setUserData(newUserData);
+      console.timeEnd("handleSubmit");
+      handleCalc(newUserData, newWeights);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message); // Use the error message
+      } else {
+        setError("An unknown error occurred");
+      }
+      console.error("Fetch error:", error);
+    }
+  };
+
+  const handleCalc = async (
+    newCharacterData: UserData,
+    newWeights: WeightsData
+  ) => {
+    const newTeamData = calculateRVs(newCharacterData, newWeights);
+    const newRollValues = newTeamData.allCharacterRVs;
+    const updatedWeights = newTeamData.weights;
+
     localStorage.setItem("rollValues", JSON.stringify(newRollValues));
-    setCharacterData(data);
     setRollValues(newRollValues);
+    localStorage.setItem("weights", JSON.stringify(updatedWeights));
+    setWeights(updatedWeights);
     setButtonDisabled(false);
+    return newRollValues;
   };
 
   const toggleExpand = () => {
+    // For the extra info box
     setIsExpanded(!isExpanded);
   };
 
-  const handleRecalc = () => {
-    onClick(
-      uid,
-      setUid,
-      setError,
-      setLoading,
-      setCharacterData,
-      weights,
-      setRollValues
-    );
-  };
-
-  const handleResetWeights = () => {
-    const weightsTable = new weightsStartingTable();
-    localStorage.setItem("weights", JSON.stringify(weightsTable.teams));
-    setWeights(weightsTable.teams);
-    //setRollValues(calculateRVs(characterData, weights));
-    onClick(
-      uid,
-      setUid,
-      setError,
-      setLoading,
-      setCharacterData,
-      weightsTable.teams,
-      setRollValues
-    );
+  const handleResetWeights = async () => {
+    try {
+      const response = await fetch(weightsFile);
+      if (!response.ok) {
+        throw new Error("Failed to load weights file");
+      }
+      const data: WeightsData = await response.json();
+      setWeights(data);
+      localStorage.setItem("weights", JSON.stringify(data));
+    } catch (error) {
+      console.error("Error loading weights:", error);
+    }
   };
 
   const clearLocalStorage = () => {
     localStorage.clear();
     setRollValues([]);
-    setCharacterData([]);
+    setUserData(null as unknown as UserData);
     //setMessage('Local storage cleared');
     setButtonDisabled(true);
   };
@@ -117,21 +124,21 @@ const HomePage = () => {
 
           <div className="flex justify-center items-center mt-6">
             <input // UID entry field
-              type="text"
+              type="number"
               placeholder="Enter UID"
               value={uid}
               onChange={(event) => setUid(event.target.value)}
               onKeyDown={(event) => {
                 if (event.key === "Enter") {
                   // Enter or click to submit
-                  handleSubmit();
+                  handleSubmit(uid, weights);
                 }
               }} // Input text box design
               className="bg-white text-black text-lg font-semibold text-center border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 rounded-lg px-4 py-2 max-w-sm"
             />
 
             <button
-              onClick={handleSubmit}
+              onClick={() => handleSubmit(uid, weights)}
               className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-200 text-lg font-semibold"
             >
               Submit
@@ -155,11 +162,11 @@ const HomePage = () => {
 
       <section className="character-section py-2">
         <h2 className="mt-2 mb-4 text-4xl font-bold text-white text-center">
-          Character Data
+          Character Artifact % of Max
         </h2>
         <div className="overflow-x-auto mx-auto">
           <div className="w-full max-w-4xl mx-auto overflow-hidden rounded-lg">
-            {characterData && (
+            {rollValues && (
               <table className="table-auto w-full">
                 <thead className="bg-gray-600">
                   <tr className="text-white uppercase">
@@ -192,16 +199,16 @@ const HomePage = () => {
                     rollValues.map((character, index) => (
                       <tr key={index} className=" bg-white ">
                         <td className="py-3 px-6 text-right text-black font-semibold whitespace-nowrap bg-gray-300 border-r border-t border-gray-100">
-                          {character[0]}
+                          {character.name}
                         </td>
-                        {character.slice(1).map((rv, artifactSlot) => {
+                        {character.rollValues.map((rv, artifactSlot) => {
                           // Remove name from array then make row of colored RVs
                           const red = 255 - (Number(rv) / 100) * 255; // Lower number is more red
                           const green = (Number(rv) / 100) * 255;
                           const alpha = Math.exp((-2.71828 * Number(rv)) / 100); // Euler number log scaling. Alpha up with lower number
                           const bgColor = `rgba(${red},${green},0,${alpha})`;
                           const borderStyle =
-                            artifactSlot === character.length - 2
+                            artifactSlot === character.rollValues.length - 1
                               ? "2px solid #172554"
                               : "none";
                           return (
@@ -246,32 +253,22 @@ const HomePage = () => {
         {isExpanded && (
           <ul className="list-disc p-6 space-y-2 text-white">
             <li>
-              Characters have a table with weightings based on how much a single
-              max value substat roll has/will increase their total damage.
-            </li>
-            <li>
-              The starting values for a character come from their most popular
-              team and rotation but those weights can be adjusted below.
-            </li>
-            <li>
-              The weightings are used to calculate a maximum value for an
-              artifact slot, and the equipped artifact strength is given as a
-              percentage of that maximum.
-            </li>
-            <li>Characters are then ranked by an average gear score.</li>
-            <li>
-              Character data, this roll values table and your edited weights
-              table are stored on your browser
+              Characters have an editable table with weights based on how much a
+              max value roll of each substat has/will increase their total
+              damage. The weightings are used to calculate a maximum value for
+              an artifact slot, and the equipped artifact strength is given as a
+              percentage of that maximum. Characters are then ranked by an
+              average gear score. Character data as well as the roll values and
+              edited weights tables are stored on your browser.
             </li>
           </ul>
         )}
       </div>
-
       <h2 className="mt-8 mb-4 text-4xl font-bold text-white text-center">
         Editable Weights
       </h2>
       <div className="overflow-x-auto mx-auto">
-        {characterData.length > 0 && (
+        {weights && (
           <table className="w-full max-w-[900px] mx-auto rounded-lg overflow-hidden">
             <thead>
               <tr className="bg-gray-600 text-white uppercase leading-normal ">
@@ -290,20 +287,23 @@ const HomePage = () => {
             </thead>
 
             <tbody>
-              {weights &&
-                Object.entries(weights).map(
-                  ([character, values], characterIndex) => (
-                    <tr key={characterIndex}>
+              {rollValues.length > 0 &&
+                rollValues.map((avatar, index) => {
+                  const avatarId = avatar.id;
+                  const characterData = weights[avatarId];
+
+                  return (
+                    <tr key={index}>
                       <td className="py-3 px-6 text-center text-white font-semibold border-r border-gray-300 bg-gray-600 h-full">
-                        {character}
+                        {characterData.name}
                       </td>
-                      {values.map((value, index) => (
+                      {characterData.weights.map((value, index) => (
                         <td key={index} style={{ height: "100%", padding: 0 }}>
                           <input
                             type="number"
                             value={value}
                             onChange={(event) =>
-                              handleChange(character, index, event, setWeights)
+                              handleChange(avatarId, index, event, setWeights)
                             }
                             onWheel={(event) => event.currentTarget.blur()}
                             aria-label="Character damage % improvement for substat: "
@@ -317,8 +317,8 @@ const HomePage = () => {
                         </td>
                       ))}
                     </tr>
-                  )
-                )}
+                  );
+                })}
             </tbody>
           </table>
         )}
@@ -326,8 +326,12 @@ const HomePage = () => {
       <div className="flex justify-center mt-4">
         <button
           onClick={() => {
-            handleRecalc();
-            window.scrollTo({ top: 0, behavior: "smooth" });
+            if (userData) {
+              handleCalc(userData, weights);
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            } else {
+              console.error("userData is undefined");
+            }
           }}
           className="px-4 py-2 m-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-200 text-lg font-semibold"
         >
@@ -360,24 +364,5 @@ const HomePage = () => {
     </div>
   );
 };
-
-class weightsStartingTable {
-  public teams: WeightsTable;
-
-  constructor() {
-    this.teams = {
-      Alhaitham: [0, 0.3, 0, 0, 0.8, 0, 2.2, 2.3, 0, 1.1],
-      Arlecchino: [0, 0.49, 0, 0, 1.51, 0, 2.95, 2.33, 0, 1.33],
-      "Hu Tao": [0.62, 0.5, 0, 1.88, 1.07, 0, 3.2, 2.16, 0, 2.36],
-      Shougun: [0, 0.53, 0, 0, 1.34, 0, 2.92, 2.43, 1.1, 0],
-      Fischl: [0, 0.63, 0, 0, 1.41, 0, 2.62, 2.88, 0, 1.43],
-      Furina: [0.93, 0, 0, 2.78, 0, 0, 1.19, 2.58, 0, 0],
-      Eula: [0, 0.79, 0, 0, 2.4, 0, 1.57, 2.36, 0, 0],
-    };
-  }
-  getTeams(): WeightsTable {
-    return this.teams;
-  }
-}
 
 export default HomePage;
---
+++ pages/api/fetchData.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const uid = req.query.uid;
+    console.log(`https://enka.network/api/uid/${uid}`);
+    const response = await fetch(`https://enka.network/api/uid/${uid}`, {
+      headers: {
+        "User-Agent": "genshin_artifact_appraiser_${uid}",
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    if (!response.ok) {
+      switch (response.status) {
+        case 400:
+          throw new Error("Invalid UID format. Please check your UID.");
+        case 404:
+          throw new Error("Player does not exist. Please verify the UID.");
+        case 424:
+          throw new Error("Game maintenance in progress. Try again later.");
+        case 429:
+          throw new Error("Rate-limited. Please try again after some time.");
+        case 500:
+          throw new Error("Server error. Please try again later.");
+        case 503:
+          throw new Error("Service unavailable. Please check back later.");
+        default:
+          throw new Error(`Unexpected error: ${response.statusText}`);
+      }
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
